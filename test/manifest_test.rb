8e8f480b3ebdfb9634766c090e85b5ab8fefa2d5
ParamagicDev <konnor5456@gmail.com>
2022-02-05 20:51:26 -0500

    continued work on asset_mapper

---
+++ test/manifest_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class AssetMapperTest < Minitest::Test
+  # def test_that_it_finds_a_file_in_the_manifest
+  #   manifest_files = %w[public/manifest.json]
+  #   AssetMapper.configure do |config|
+  #     config.manifest_files = manifest_files
+  #     config.cache_manifest = cache_manifest
+  #   end
+  # end
+end
