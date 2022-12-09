d6ab2e044b74ba52162ac8b1b6ff773012092f39
Konnor Rogers <konnor5456@gmail.com>
2022-12-09 08:39:16 -0500

    remove esbuilds

--- test/rails/asset-mapper-app/public/esbuild-builds/academic-cap-J454ZWXQ.js
+++
@@ -1,6 +0,0 @@
-// app/javascript/assets/academic-cap.svg
-var academic_cap_default = "./academic-cap-3WMCWJAT.svg";
-export {
-  academic_cap_default as default
-};
-//# sourceMappingURL=academic-cap-J454ZWXQ.js.map
