f073d8f64daa294e96e0be40ff61b624a05d8047
Konnor Rogers <konnor5456@gmail.com>
2022-12-09 13:46:24 -0500

    more esbuild work

--- test/rails/asset-mapper-app/esbuild.config.js
+++ test/rails/asset-mapper-app/esbuild.config.js
@@ -1,17 +1,22 @@
 // esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds --public-path=assets
 // @ts-check
-
 //
-/** @typedef BuildResult import("esbuild").BuildResult  */
-(async () => {
-  const path = require("path");
-  const process = require("process");
-  const esbuild = require("esbuild");
-  const fsLib = require("fs");
-  const fs = fsLib.promises;
+const path = require("path");
+const process = require("process");
+const esbuild = require("esbuild");
+const fsLib = require("fs");
+const fs = fsLib.promises;
 
-  /** @type {import("esbuild").Plugin} **/
-  let plugin = {
+/**
+ * @param {{ outputRoot?: string, entrypointRoot?: string }} [options={}] Plugin options.
+ * @return {import("esbuild").Plugin}
+ */
+function AssetMapperManifest(options = {}) {
+  if (options == null) options = {};
+
+  const { outputRoot = "public", entrypointRoot = "app/javascript" } = options;
+
+  return {
     name: "asset-mapper-manifest",
 
     setup(build) {
@@ -20,64 +25,80 @@
       // assume that the user wants to hash their files by default,
       // but don't override any hashing format they may have already set.
       ["entryNames", "assetNames", "chunkNames"].forEach((str) => {
-      	if (build.initialOptions[str]) return
+        if (build.initialOptions[str]) return;
 
-				if (str === "chunkNames") {
-					build.initialOptions[str] = "chunks/[name]-[hash]"
-					return
-				}
+        if (str === "chunkNames") {
+          build.initialOptions[str] = "chunks/[name]-[hash]";
+          return;
+        }
 
-				build.initialOptions[str] = "[dir]/[name]-[hash]"
-      })
+        build.initialOptions[str] = "[dir]/[name]-[hash]";
+      });
 
       build.onEnd(async (result) => {
         if (!result.metafile) {
-          console.warn("No metafile found from ESBuild. No manifest generated.")
-          return
+          console.warn(
+            "No metafile found from ESBuild. No manifest generated."
+          );
+          return;
         }
 
         if (!result.metafile.outputs) {
-          console.warn("No inputs found. Make sure you are passing entrypoints to ESBuild.")
-          return
+          console.warn(
+            "No inputs found. Make sure you are passing entrypoints to ESBuild."
+          );
+          return;
         }
 
         /** @type Map<string, string> */
         const manifest = new Map();
 
-				const outfileDir = build.initialOptions.outfile ? build.initialOptions.outfile : null
-        const outdir = build.initialOptions.outdir || outfileDir || process.cwd()
-
-				const outputRoot = "public/"
-				const entrypointRoot = "app/javascript/"
+        const outfileDir = build.initialOptions.outfile
+          ? build.initialOptions.outfile
+          : null;
+        const outdir =
+          build.initialOptions.outdir || outfileDir || process.cwd();
 
         // Let's loop through all the various outputs
         for (const hashedPath in result.metafile.outputs) {
           const output = result.metafile.outputs[hashedPath];
 
           if (!output.entryPoint) {
-            continue
+            continue;
           }
 
-					// Replace the relative path. We don't need "/public"
-					const entryPoint = output.entryPoint
+          // Replace the relative path. We don't need "/public"
+          const entryPoint = output.entryPoint;
 
-          manifest.set(path.relative(entrypointRoot, entryPoint), path.relative(outputRoot, hashedPath))
+          manifest.set(
+            path.relative(entrypointRoot, entryPoint),
+            path.relative(outputRoot, hashedPath)
+          );
 
-          if (!output.cssBundle) continue
+          if (!output.cssBundle) continue;
 
-					const { dir, name } = path.parse(entryPoint)
-          const cssBundle = path.join(dir, name + ".css")
-          manifest.set(path.relative(entrypointRoot, cssBundle), path.relative(outputRoot, output.cssBundle))
+          const { dir, name } = path.parse(entryPoint);
+          const cssBundle = path.join(dir, name + ".css");
+          manifest.set(
+            path.relative(entrypointRoot, cssBundle),
+            path.relative(outputRoot, output.cssBundle)
+          );
         }
 
-        const manifestFolder = path.resolve(outdir)
-        await fs.mkdir(manifestFolder, { recursive: true })
-        await fs.writeFile(path.join(manifestFolder, "asset-mapper-manifest.json"), JSON.stringify(Object.fromEntries(manifest), null, 2))
+        const manifestFolder = path.resolve(outdir);
+        await fs.mkdir(manifestFolder, { recursive: true });
+        await fs.writeFile(
+          path.join(manifestFolder, "asset-mapper-manifest.json"),
+          JSON.stringify(Object.fromEntries(manifest), null, 2)
+        );
       });
     },
   };
+}
 
-  // const key = path.join(process.cwd(), "app", "javascript", "application.js")
+/** @typedef BuildResult import("esbuild").BuildResult  */
+(async () => {
+  // const key = path.join("application")
   // const value = path.join(".", process.cwd(), "app", "javascript", "application.js")
   await esbuild.build({
     entryPoints: {
@@ -101,9 +122,7 @@
       ".json": "file",
     },
     metafile: true,
-    // absWorkingDir: process.cwd(),
-    assetNames: "[dir]",
     outdir: path.join(process.cwd(), "public/esbuild-builds"),
-    plugins: [plugin],
+    plugins: [AssetMapperManifest()],
   });
 })();
--- test/rails/asset-mapper-app/public/esbuild-builds/chunks/academic-cap-KFOUOWNQ.js
+++
@@ -1,6 +0,0 @@
-// app/javascript/assets/academic-cap.svg
-var academic_cap_default = "../app/javascript/assets.svg";
-export {
-  academic_cap_default as default
-};
-//# sourceMappingURL=academic-cap-KFOUOWNQ.js.map
